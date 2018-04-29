dbf1edfa286bc272f2c1178c2d6f2010383a424e
andrese <andreseloysv@gmail.com>
2018-04-29 18:38:40 +0200

    submit button

--- save_poll.php
+++ save_poll.php
@@ -13,7 +13,7 @@ $json_obj = json_decode($json_str);
 $questionList = $json_obj->questionList;
 $questionIdClear = [];
 $sql = "";
-var_dump($questionList);
+
 $id_respondent = 1;
 foreach($questionList as $question){
     $questionIdClear = preg_replace('/[^a-zA-Z0-9]/', '',$question->id);
@@ -28,18 +28,11 @@ foreach($questionList as $question){
     $id_respondent);";
 }
 
-//exit();
-//$id_respondent = preg_replace('/[^a-zA-Z0-9]/', '', $_POST["id_respondent"]);
-
 $conn = new mysqli($server, $username, $password, $db);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-
-
-
-
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
